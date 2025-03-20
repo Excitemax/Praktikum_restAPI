77134f336904626566b359a36facd9478b7796a1
Excitemax <94905481+Excitemax@users.noreply.github.com>
2025-03-20 15:12:29 +0800

    memakai library express

--- index.js
+++ index.js
@@ -2,22 +2,20 @@ import http from 'http';
 import mysql from 'mysql2';
 
 const db = mysql.createConnection({ host: "localhost", user: "root", database: "openapi", password: ""});
+const app = express();
 
-const server = http.createServer((req, res) => {
+app.get('/users', (req, res) => {
     db.query('SELECT * FROM user', (err, results) => {
         if (err) {
-            res.writeHead(500, {'Content-Type': 'text/plain '});
-            res.end('Internal Server Error');
+            res.status(500).send('Internal Server Error');
             return;
         }
 
+        res.json(results);
 
-    
-    res.writeHead(200, { 'Content-Type': 'application/json'});
-    res.end(JSON.stringify(results));
-    })
+    });
 });
 
-server.listen(3000, () => console.log('server berjalan di http://localhost:3000'));
+app.listen(3000, () => console.log('server berjalan di http://localhost:3000'));
 
 
